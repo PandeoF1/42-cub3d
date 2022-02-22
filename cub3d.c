a1461fbb8abfd615ac93d7ed4da8d27b45913f75
adonis <adonissfy42@gmail.com>
2022-02-22 14:51:17 +0100

    45r

--- cub3d.c
+++ cub3d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
+/*   By: asaffroy <asaffroy@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 12:32:17 by tnard             #+#    #+#             */
-/*   Updated: 2022/02/22 10:33:05 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2022/02/22 13:04:45 by asaffroy         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,29 @@ int main(int argc, char *argv[])
 {
 	int			x;
 	t_map_check *check;
-
+	void		*mlx;
+	void		*mlx_win;
+	int y;
 	x = 0;
 	check = malloc(sizeof(t_map_check));
 	if (ft_check_arg(argc, argv, check))
 	{
 		while (check->map[x])
 			ft_printf("```%s```\n", check->map[x++]);
-		/*x = -1;
+		x = -1;
 		y = -1;
 		mlx = mlx_init();
 		
-		mlx_win = mlx_new_window(mlx,
-								 check->max_x * 50,
-								 (ft_splitlen(check->map)) * 50, "so_long | v1.0");
-		graphic.mlx = mlx;
-		check->axis = 0;
-		graphic.mlx_win = mlx_win;
-		graphic.map_check = check;
+		mlx_win = mlx_new_window(mlx, 1024, 512, "so_long | v1.0");
+		// graphic.mlx = mlx;
+		// check->axis = 0;
+		// graphic.mlx_win = mlx_win;
+		// graphic.map_check = check;
 
-		mlx_key_hook(graphic.mlx_win, ft_win_event, &graphic);
-		mlx_loop_hook(graphic.mlx, ft_sprite, &graphic);
+		// mlx_key_hook(graphic.mlx_win, ft_win_event, &graphic);
+		// mlx_loop_hook(graphic.mlx, ft_sprite, &graphic);
 
-		mlx_loop(mlx);*/
+		mlx_loop(mlx);
 	}
 	else
 		ft_printf("Error\n");
