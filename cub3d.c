bc640902d3631560e1a8845277d218c0ddf8155b
tnard <theo.nard18@gmail.com>
2022-02-22 14:50:23 +0100

    test

--- cub3d.c
+++ cub3d.c
@@ -6,16 +6,43 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 12:32:17 by tnard             #+#    #+#             */
-/*   Updated: 2022/02/22 10:33:05 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2022/02/22 14:50:10 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/cub3d.h"
 
+void	ft_free_check(t_map_check *check)
+{
+	if (check->map)
+		ft_free_split(check->map);
+	if (check->n)
+		free(check->n);
+	if (check->s)
+		free(check->s);
+	if (check->e)
+		free(check->e);
+	if (check->w)
+		free(check->w);
+	if (check->f)
+		free(check->f);
+	if (check->c)
+		free(check->c);
+	free(check);
+}
+
+void	ft_refresh(t_graphic *graphic)
+{
+	ft_printf("Debug: refresh\n");
+}
+
 int main(int argc, char *argv[])
 {
 	int			x;
-	t_map_check *check;
+	void		*mlx;
+	void		*mlx_win;
+	t_map_check	*check;
+	t_graphic	graphic;
 
 	x = 0;
 	check = malloc(sizeof(t_map_check));
@@ -23,6 +50,15 @@ int main(int argc, char *argv[])
 	{
 		while (check->map[x])
 			ft_printf("```%s```\n", check->map[x++]);
+
+		mlx = mlx_init();
+		
+		mlx_win = mlx_new_window(mlx,
+								50,
+								 50, "so_long | v1.0");
+		//ft_refresh(&graphic);
+		ft_printf("uwu\n");
+		mlx_loop(mlx);
 		/*x = -1;
 		y = -1;
 		mlx = mlx_init();
@@ -42,17 +78,6 @@ int main(int argc, char *argv[])
 	}
 	else
 		ft_printf("Error\n");
-	free(check);
+	ft_free_check(check);
 	return (0);
 }
-/* for (y = -r; y < r+1; y++)  {   
-                printf("\n");       
-                for (x = -r; x < r+1 ; x++) {            
-                         if(abs((x * x) + (y * y)-(r * r)) <= (r/2)) {                     
-                                printf(" * ");                                         
-                        }
-                         else  {                                              
-                                printf("   ");                       
-                        }         
-                }                
-            } */
