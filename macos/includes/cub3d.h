d722e7984c2a715eea9b60f6da30ab9e2856fb0f
tnard <theo.nard18@gmail.com>
2022-04-12 10:57:13 +0200

    makefile

--- macos/includes/cub3d.h
+++ macos/includes/cub3d.h
@@ -6,14 +6,14 @@
 /*   By: tnard <tnard@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 02:38:07 by tnard             #+#    #+#             */
-/*   Updated: 2022/04/12 09:38:52 by tnard            ###   ########lyon.fr   */
+/*   Updated: 2022/04/12 10:55:26 by tnard            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../mlbx/mlx.h"
+# include "../mlx/mlx.h"
 # include "../libft/libft.h"
 # include "../ft_printf/ft_printf.h"
 # include <stdio.h>
